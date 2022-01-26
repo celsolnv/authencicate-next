bdcca6a34339a1fcb5425f34c3f61deac406eb9c
leo <celsolnv27@gmail.com>
2022-01-26 10:34:40 -0300

    feature: create hook and component for validate permission for user

---
+++ hooks/useCan.ts
@@ -0,0 +1,35 @@
+import { AuthContext } from "./../contexts/AuthContext";
+import { useContext } from "react";
+type UseCamParams = {
+  permissions?: string[];
+  roles?: string[];
+};
+
+export function useCan({ permissions, roles }: UseCamParams) {
+  const { user, isAuthenticated } = useContext(AuthContext);
+
+  if (!isAuthenticated) {
+    return false;
+  }
+
+  if (permissions?.length > 0) {
+    const hasAllPermissions = permissions.every((permission) => {
+      return user.permissions.includes(permission);
+    });
+
+    if (!hasAllPermissions) {
+      return false;
+    }
+  }
+  if (roles?.length > 0) {
+    const hasAllRoles = roles.some((role) => {
+      return user.roles.includes(role);
+    });
+
+    if (!hasAllRoles) {
+      return false;
+    }
+  }
+
+  return true;
+}
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from "next"
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
 import { AuthContext } from "../contexts/AuthContext"
 import { setupApi } from "../services/api"
 import { api } from "../services/apiClient"
@@ -8,14 +9,19 @@ import { withSSRAuth } from "../utils/withSSRAuth"
 export default function Dashboard(){
   const {user} = useContext(AuthContext)
 
+
   useEffect(()=>{
     api.get("/me").then(response=>{
       console.log("Dash", response.data)
     }).catch((error)=>console.log(error))
   },[])
+  
   return(
     <div>
       <h1>Dashboard: {user?.email}</h1>
+      <Can permissions={['metrics.list']}>
+        <h2>Metrics</h2>
+      </Can>
     </div>
   )
 }
